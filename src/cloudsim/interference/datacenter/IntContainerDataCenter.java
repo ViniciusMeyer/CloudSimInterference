eda0c6e2cad6d479d9b7480692ce357b961a8668
viniciusmeyer <vinicius.meyer.001@acad.pucrs.br>
2020-08-05 14:41:14 -0300

    update

--- src/cloudsim/interference/datacenter/IntContainerDataCenter.java
+++ src/cloudsim/interference/datacenter/IntContainerDataCenter.java
@@ -1100,7 +1100,8 @@ public class IntContainerDataCenter extends SimEntity {
 		List<Solution> solutionList1 = new ArrayList<Solution>(); // adapt
 		Solution nextSolution = new Solution();
 		
-		int interval = 600, start = 1, end = 0, total = 700, count = 1;
+		int interval = 600, start = 1, end = 0, total = 7200
+				, count = 1;
 
 		for (int second = 1; second <= total; second++) {
 			
