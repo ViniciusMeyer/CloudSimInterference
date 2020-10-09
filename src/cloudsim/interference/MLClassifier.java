f8bf1df4de0e3facf7c6d96bcaf827f9a77e8c88
vinim <vinim@DESKTOP-OTD5GFM>
2020-10-09 01:13:33 -0300

    update

--- src/cloudsim/interference/MLClassifier.java
+++ src/cloudsim/interference/MLClassifier.java
@@ -96,7 +96,7 @@ public class MLClassifier {
 			re.eval(".libPaths('/home/vinicius/R/x86_64-pc-linux-gnu-library/3.6')"); // ubuntu
 		}
 		if (hostname.equals("DESKTOP-OTD5GFM")) {
-			project_folder = "C:/Users/vinim/eclipse-workspace/IntegrationRandJava/R/"; //windows
+			project_folder = "C:/Users/vinim/git/CloudSimInterference/R/"; //windows
 			re.eval(".libPaths('c:/users/vinim/Documents/R/win-library/3.5')"); //windows	
 		}
 		if (hostname.equals("pantana01")) {
